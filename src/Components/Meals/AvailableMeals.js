2be110520b4df69a659f66e9e24e4d75387afe10
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2022-09-15 21:44:44 +0200

    meals are now fetched from an external api

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,18 +1,38 @@
-import React from "react";
+import { useEffect, useState, useCallback } from "react";
 
-import { DUMMY_MEALS } from "../../dummy-meals";
 import Card from "../UI/Card";
 
 import classes from "./AvailableMeals.module.css";
 import MealListItem from "./MealListItem";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <MealListItem key={meal.id} meal={meal} />);
+  const [meals, setMeals] = useState([]);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+  const mealsList = meals.map((meal) => <MealListItem key={meal.id} meal={meal} />);
+
+  const fetchData = useCallback(async () => {
+    setError(null);
+    try {
+      const res = await fetch("https://http-hook-60231-default-rtdb.firebaseio.com/meals.json");
+      const fetchedData = await res.json();
+      setMeals(fetchedData);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {loading && !error && <span>Loading...</span>}
+        {!loading && error && <span>An Error Ocurred: {error}</span>}
+        {!loading && !error && <ul>{mealsList}</ul>}
       </Card>
     </section>
   );
